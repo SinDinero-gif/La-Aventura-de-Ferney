34b1c21888a49de273b9960aac543fdd0d4ccbfc
SinDinero-gif <samuposadalds@gmail.com>
2024-05-25 11:30:21 -0500

    Update Scriptable Objects

--- Assets/Scripts/Data/EntityData.cs
+++ Assets/Scripts/Data/EntityData.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Entities", menuName = "ScriptableObjects/Attack", order = 1)]
+[CreateAssetMenu(fileName = "Attack", menuName = "ScriptableObjects/Entities", order = 1)]
 public class EntityData : ScriptableObject
 {
     public string Id;
 
     public float attackDamage;
+
+    public float attackRadius;
+
+    public float attackSpeed;
+
+    public LayerMask enemyLayers;
+
+    public bool canAttack;
+
+    private int _maxHealth;
+
+    public int _currentHealth;
+
+    public Animator _animator;
+
 }
